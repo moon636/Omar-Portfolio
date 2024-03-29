76b7b7b16a6079e595c0b704dc35f0f431092705
Ahmed S <ahmedscriptx@gmail.com>
2024-03-29 16:28:53 +0000

    developing video carousel

--- index.js
+++ index.js
@@ -1,20 +1,33 @@
 const leftArrow = document.getElementById("left-arr")
 const rightArrow = document.getElementById("right-arr")
 
-const video1 = document.getElementById("video-1")
-const video2 = document.getElementById("video-2")
-const video3 = document.getElementById("video-3")
-const video4 = document.getElementById("video-4")
-const video5 = document.getElementById("video-5")
+let video1 = document.getElementById("video-1")
+let video2 = document.getElementById("video-2")
+let video3 = document.getElementById("video-3")
+let video4 = document.getElementById("video-4")
+let video5 = document.getElementById("video-5")
 
 
-leftArrow.onclick = function() {
+function toggleControls(video,bool) {
+
+    if (bool) {
+        video.setAttribute("controls","controls") 
+        }
+    else {
+        if (video.hasAttribute("controls")) {
+            video.removeAttribute("controls")  
+        } 
+    }
+}
+
+rightArrow.onclick = function() {
 
 
-    video2.style.left = "-30%"
+    video1.style.left = "102%"
+    video2.style.left = "-33%"
     video3.style.left = "0"
     video4.style.left = "32.5%"
-    video5.style.right = "0"
+    video5.style.left = "69%"
 
     video3.style.width = "31%"
     video3.style.height = "80%"
@@ -24,5 +37,77 @@ leftArrow.onclick = function() {
     video4.style.height = "100%"
     video4.style.top = "0%"   
 
-    video3.style.zIndex = 75
+    toggleControls(video3,false)
+    toggleControls(video4,true)
+
+    video1.pause();
+    video1.currentTime = 0;
+    video2.pause();
+    video2.currentTime = 0;
+    video3.pause();
+    video3.currentTime = 0;
+    video4.pause();
+    video4.currentTime = 0;
+    video5.pause();
+    video5.currentTime = 0;
+
+    video1.style.zIndex = 20
+    video2.style.zIndex = 25
+    video3.style.zIndex = 25
+    video4.style.zIndex = 75
+    video5.style.zIndex = 25
+
+    let tempVideo1 = video1
+    
+    video1 = video2
+    video2 = video3
+    video3 = video4
+    video4 = video5
+    video5 = tempVideo1
+};
+
+leftArrow.onclick = function() {
+
+
+    video1.style.left = "0"
+    video2.style.left = "32.5%"
+    video3.style.left = "69%"
+    video4.style.left = "102%"
+    video5.style.left = "-33%"
+
+    video3.style.width = "31%"
+    video3.style.height = "80%"
+    video3.style.top = "10%"
+
+    video2.style.width = "35%"
+    video2.style.height = "100%"
+    video2.style.top = "0%"   
+
+    toggleControls(video3,false)
+    toggleControls(video2, true)
+
+    video1.pause();
+    video1.currentTime = 0;
+    video2.pause();
+    video2.currentTime = 0;
+    video3.pause();
+    video3.currentTime = 0;
+    video4.pause();
+    video4.currentTime = 0;
+    video5.pause();
+    video5.currentTime = 0;
+    
+    video1.style.zIndex = 25
+    video2.style.zIndex = 75
+    video3.style.zIndex = 25
+    video4.style.zIndex = 25
+    video5.style.zIndex = 20
+
+    let tempVideo5 = video5
+    
+    video5 = video4
+    video4 = video3
+    video3 = video2
+    video2 = video1
+    video1 = tempVideo5
 };
